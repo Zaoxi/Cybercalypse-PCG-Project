89f43ca72e24dd1d7bdc697996fc5738ac2014d2
Zaoxi <whgmlwo1000@gmail.com>
2018-06-23 03:04:07 +0900

    CGridGenerator 클래스 필드 명 일부 수정
    - CGridDrivenContentsGenerator의 필드 명 일부 수정

--- CybercalypseTest Project/CybercalypseTest/Assets/Scripts/CGridDrivenContentsGenerator.cs
+++ CybercalypseTest Project/CybercalypseTest/Assets/Scripts/CGridDrivenContentsGenerator.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class CGridDrivenContentsGenerator : MonoBehaviour {
+    // 상대 좌표에 곱할 상수
+    const float TILE_LENGTH = 0.16f;
     // 해당하는 상대좌표에 어떤 종류의 타일이 배치되었는지 저장
     private Dictionary<Vector2Int, ETileType> tilePosition;
     // 그리드 사각형 내에 어떤 그리드에 어떤 Chamber가 위치하는지 저장
@@ -12,8 +14,8 @@ public class CGridDrivenContentsGenerator : MonoBehaviour {
     // 각 Chamber에 대한 공통 정보
     private float chamberWidth;
     private float chamberHeight;
-    private int numOfChamberInWidth;
-    private int numOfChamberInHeight;
+    private int numOfChamberInHorizontal;
+    private int numOfChamberInVertical;
 
     // 출발 지점의 Chamber 상대 좌표
     private Vector2Int startChamber;
@@ -26,12 +28,12 @@ public class CGridDrivenContentsGenerator : MonoBehaviour {
         chamberPosition = new Dictionary<Vector2Int, Chamber>();
     }
     // 생성할 맵에 대한 최소 정보 입력(Chamber의 가로, 세로, Chamber의 가로 개수, Chamber의 세로 개수)
-    public void InitGenerator(float width, float height, int numOfWidth, int numOfHeight)
+    public void InitGenerator(float width, float height, int numOfHorizontal, int numOfVertical)
     {
         chamberWidth = width;
         chamberHeight = height;
-        numOfChamberInWidth = numOfWidth;
-        numOfChamberInHeight = numOfHeight;
+        numOfChamberInHorizontal = numOfHorizontal;
+        numOfChamberInVertical = numOfVertical;
         makeNoneChamber();
     }
     // 맵 구동기 가동
@@ -43,9 +45,9 @@ public class CGridDrivenContentsGenerator : MonoBehaviour {
     // 정해진 그리드 내에 None Chamber를 생성
     private void makeNoneChamber()
     {
-        for(int i=0; i<numOfChamberInHeight; i++)
+        for(int i=0; i<numOfChamberInVertical; i++)
         {
-            for(int j=0; j<numOfChamberInWidth; j++)
+            for(int j=0; j<numOfChamberInHorizontal; j++)
             {
                 chamberPosition.Add(new Vector2Int(i, j), new Chamber(EChamberType.None));
             }
@@ -57,7 +59,7 @@ public class CGridDrivenContentsGenerator : MonoBehaviour {
         Vector2Int currentPosition, nextPosition;
         Vector2Int[] adjacentPosition;
         // 필수 경로 시작 지점
-        currentPosition = startChamber = new Vector2Int((int)Random.Range(0.0f, numOfChamberInHeight), 0);
+        currentPosition = startChamber = new Vector2Int((int)Random.Range(0.0f, numOfChamberInVertical), 0);
         chamberPosition[currentPosition].SetChamberType(EChamberType.Essential);
         // 필수 경로 인접 지점
         adjacentPosition = getAdjacentPath(currentPosition);
@@ -66,7 +68,7 @@ public class CGridDrivenContentsGenerator : MonoBehaviour {
         addFromCurrentToNextChamberPassage(currentPosition, nextPosition);
         currentPosition = nextPosition;
         // 그리드의 맨 오른쪽 위치 까지 진행
-        while (currentPosition.x != numOfChamberInWidth - 1)
+        while (currentPosition.x != numOfChamberInHorizontal - 1)
         {
             adjacentPosition = getAdjacentPath(currentPosition);
             nextPosition = adjacentPosition[(int)Random.Range(0.0f, adjacentPosition.Length)];
