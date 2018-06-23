5b2f14384e36dcd816171206ecc1b2c5b72cda1b
Zaoxi <whgmlwo1000@gmail.com>
2018-06-23 20:08:32 +0900

    필수 경로 에러 수정
    - Chamber 구조체를 CChamber 클래스로 변경

---
+++ CybercalypseTest Project/CybercalypseTest/Assets/Scripts/CChamber.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class CChamber
+{
+    // 주변 방향 정보 리스트
+    public List<Vector2Int> NextChamberPosition { get; private set; }
+    // Chamber의 종류
+    public EChamberType ChamberType { get; set; }
+
+    public CChamber(EChamberType type)
+    {
+        NextChamberPosition = new List<Vector2Int>();
+        ChamberType = type;
+    }
+}
